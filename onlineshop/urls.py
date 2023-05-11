2ac406fd2c521d9ddd9d23cb4608ed613472d346
XOLIXG <zlobovroma@gmail.com>
2023-05-11 21:41:47 +0500

    Beautiful front

--- onlineshop/urls.py
+++ onlineshop/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainpage.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)#Добавить изображение
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)#Добавить изображение
